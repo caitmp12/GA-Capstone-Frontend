51ce0a4966cd224e779b76f76044abc00dacc23a
Caitlin Moore-Penaskovic <cait.moorepenaskovic@gmail.com>
2020-12-18 21:46:02 -0500

    home component shell

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import Home from "./components/Home"
 
 function App() {
   return (
     <div className="App">
       <h1>Say Hi!</h1>
+      <Home />
     </div>
   );
 }
