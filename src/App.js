971fc5eaff4aef61bf407b38e407157ca28b03b3
Caitlin Moore-Penaskovic <cait.moorepenaskovic@gmail.com>
2020-12-23 21:31:48 -0500

    component changes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import React from "react";
 import Home from "./components/Home"
+import Login from "./components/Login"
 
 function App() {
   return (
---
+++ src/components/Form.js
@@ -0,0 +1,11 @@
+import React from "react"
+import { Link } from "react-router-dom"
+
+const Form = (props) => {
+
+    return (
+        <h1>Form</h1>
+    )
+}
+
+export default Form;
