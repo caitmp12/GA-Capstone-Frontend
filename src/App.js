5e31b6e51bf942f2d5951001269fa7a87d9d5cd7
Caitlin Moore-Penaskovic <cait.moorepenaskovic@gmail.com>
2020-12-26 15:31:48 -0500

    updated routes in appjs

--- src/App.js
+++ src/App.js
@@ -4,14 +4,21 @@ import React from "react";
 import { Route, Link, Switch } from "react-router-dom"
 import Home from "./components/Home"
 import Auth from "./components/Auth"
-import Form from "./components/Form"
+import Nav from "./components/Nav"
+import Dashboard from "./components/Dashboard"
 
 
 function App() {
   return (
     <div className="App">
+      <Nav />
       <h1>Say Hi!</h1>
-      <Home />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/auth/:form" component={Auth} />
+        <Route path="/dashboard" component={Dashboard} />
+        <Home />
+      </Switch>
     </div>
   );
 }
