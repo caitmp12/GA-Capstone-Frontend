3288eff6cd799bb7153835850e8d828818dda10b
Caitlin Moore-Penaskovic <cait.moorepenaskovic@gmail.com>
2020-12-26 16:29:14 -0500

    form and form login in navjs

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,7 +1,45 @@
 import React from "react"
+import {useAppState} from "../AppState.jsx"
 
 const Auth = (props) => {
-    return <h1>Auth</h1>
+
+    const type = props.match.params.form
+    const [formData, setFormData] = React.useState({
+        username: "",
+        password: ""
+    })
+
+    const {dispatch} = useAppState()
+
+    const actions = {
+        signup: {
+            action: "signup",
+            payload: formData
+        },
+        login: {
+            action: "login",
+            payload: formData
+        }
+    }
+
+    const handleChange = (event) => {
+        setFormData(...formData, {[event.target.name]: event.target.value})
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        dispatch(actions[type])
+    }
+
+    return (
+        <div>
+            <form>
+                <input type="text" name="username" value={formData.username} onChange={handleChange} />
+                <input type="text" name="password" value={formData.password} onChange={handleChange}/>
+                <input type="submit" value={type} />
+            </form>
+        </div>
+    )
 }
 
 export default Auth
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,7 +1,17 @@
 import React from "react"
+import { Route, Link, Switch } from "react-router-dom"
 
 const Nav = (props) => {
-    return <h1>Nav</h1>
+    return (
+        <header>
+            <h1>sayHi!</h1>
+            <nav>
+                <Link to="/">Home</Link>
+                <Link to="/auth/signup">Signup</Link>
+                <Link to="/auth/login">Login</Link>
+            </nav>
+        </header>
+    )
 }
 
 export default Nav
