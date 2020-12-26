6db2408309c883761a8f94f448fe9aa6e0b2495b
Caitlin Moore-Penaskovic <cait.moorepenaskovic@gmail.com>
2020-12-26 15:02:11 -0500

    updated index.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import React from "react";
+import { Route, Link, Switch } from "react-router-dom"
 import Home from "./components/Home"
 import Auth from "./components/Auth"
 import Form from "./components/Form"
---
+++ src/AppState.jsx
@@ -0,0 +1,42 @@
+import React from "react"
+
+//Initial State
+
+const initialState = {
+    url: "http://localhost:3001"
+}
+
+//Reducer
+
+//action = {type: "", payload: ---}
+
+const reducer = (state, action) => {
+
+    switch(action.type){
+        default:
+            return state 
+    }
+
+}
+
+//App Context
+
+const AppContext = React.createContext(null)
+
+//AppState Component
+
+export const AppState = (props) => {
+
+    const [state, dispatch] = useReducer(reducer, initialState)
+
+    return <AppContext.Provider value={{state, dispatch}}>
+        {props.children}
+    </AppContext.Provider>
+
+}
+
+//useAppState Hook
+
+export const useAppState = () => {
+    return React.useContext(AppContext)
+}
