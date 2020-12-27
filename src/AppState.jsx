a4dfd2aceba78256a96e002bb28c86ef04a08707
Caitlin Moore-Penaskovic <cait.moorepenaskovic@gmail.com>
2020-12-27 10:57:07 -0500

    updated appstatejsx

--- src/AppState.jsx
+++ src/AppState.jsx
@@ -15,48 +15,61 @@ const initialState = {
 //action = {type: "", payload: ---}
 
 const reducer = (state, action) => {
-
-    switch(action.type){
-        case "signup":
-            fetch("http://localhost:3000/users/", {
-                method: "post",
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify(action.payload)
-            })
-            .then(response => response.json())
-            .then(user => {
-                return {
-                    ...state,
-                    token: user.token,
-                    username: user.username,
-                }
-            })
-        break
-        case "login":
-            fetch("http://localhost:3000/login/", {
-                method: "post",
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify(action.payload),
-            })
-                .then(response => response.json())
-                .then(user => {
-                    return {
-                        ...state,
-                        token: user.token,
-                        username: user.username,
-                    }
-                })  
-            break      
-        default:
-            return state 
+    let newState
+    switch (action.type) {
+        case "auth":
+            newState = { ...state, ...action.payload }
+            return newState
             break
+        default:
+            return state
+            break    
     }
-
 }
+
+// const reducer = (state, action) => {
+
+//     switch(action.type){
+//         case "signup":
+//             fetch("http://localhost:3000/users/", {
+//                 method: "post",
+//                 headers: {
+//                     "Content-Type": "application/json"
+//                 },
+//                 body: JSON.stringify(action.payload)
+//             })
+//             .then(response => response.json())
+//             .then(user => {
+//                 return {
+//                     ...state,
+//                     token: user.token,
+//                     username: user.username,
+//                 }
+//             })
+//         break
+//         case "login":
+//             fetch("http://localhost:3000/login/", {
+//                 method: "post",
+//                 headers: {
+//                     "Content-Type": "application/json"
+//                 },
+//                 body: JSON.stringify(action.payload),
+//             })
+//                 .then(response => response.json())
+//                 .then(user => {
+//                     return {
+//                         ...state,
+//                         token: user.token,
+//                         username: user.username,
+//                     }
+//                 })  
+//             break      
+//         default:
+//             return state 
+//             break
+//     }
+
+// }
 
 //App Context
 
